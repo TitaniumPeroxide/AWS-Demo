289028d924dae7ce642950f2ba9e41b3a0ef9f56
“Akhil <akhilj307@gmail.com>
2025-05-26 18:10:18 +0100

    Text Changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.title("My First Streamlit App")
 
-name = st.text_input("Enter your name:")
+name = st.text_input("Enter your first name:")
 
 if name:
     st.write(f"Hello, {name}! Welcome to your Streamlit app.")
